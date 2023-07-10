efa70b24dd761ffe77a6124ab2511898ec5c9bb2
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-07-10 17:01:51 +0100

    create models and migration for table of app

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -21,7 +24,21 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'phone',
+        'image'
+
     ];
+
+    public static $roleOptions = [
+        'Admin' => 'Admin',
+        'Waiter' => 'Waiter',
+        'Cashier' => 'Cashier',
+        'Kitchen' => 'Kitchen',
+
+    ];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -40,5 +57,26 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'phone' => 'string',
+
     ];
+    
+    /**
+    * Get the identifier that will be stored in the subject claim of the JWT.
+    *
+    * @return mixed
+    */
+   public function getJWTIdentifier() {
+       return $this->getKey();
+   }
+   /**
+    * Return a key value array, containing any custom claims to be added to the JWT.
+    *
+    * @return array
+    */
+   public function getJWTCustomClaims() {
+       return [];
+   }  
+
 }
